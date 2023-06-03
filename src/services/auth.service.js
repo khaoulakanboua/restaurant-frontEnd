0115a277b3453f729c74e0a26e20c7cbb30433d3
khaoulakanboua <khaoulakanboua@gmail.com>
2023-06-03 14:15:49 +0100

    t

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth/";
+const API_URL = "https://restaurant-production-f803.up.railway.app/api/auth/";
 
 class AuthService {
   login(username, password) {
